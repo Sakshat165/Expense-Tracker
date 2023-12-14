8186044dfae6c6a8399e7e128fd15b3af6a75343
Sakshat165 <sakshat.worksite@gmail.com>
2023-12-14 09:19:11 +0530

    database connected

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const connectToMongo=require('./db');
+const express = require('express');
+connectToMongo();
+var app = express()
+
+const port = 5000
+app.use( express.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+  })
